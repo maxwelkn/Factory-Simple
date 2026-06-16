471584de7e7ff07264a1ac60d12feaf4566457ed
maxwelkn <saelmaxwel@gmail.com>
2026-06-16 18:11:19 -0400

    menu reparado con while

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace FactoryProblema
     {    
         static void Main(string[] args)
         {
+            while (true) 
+            {
             Console.Clear();
-            Console.WriteLine("[1]Sedan [2]Compacto [3]Suv");
+            Console.WriteLine("[1]Sedan [2]Compacto [3]Suv [4] Salir");
             Console.Write("Elija cual tipo de vehiculo desea: ");
             int opcionNumero = int.Parse(Console.ReadLine()!);
         
@@ -15,19 +17,31 @@ namespace FactoryProblema
 
             Vehiculo v = VehiculoFactory.CrearVehiculo(opcionElegida);
             System.Console.WriteLine("\nIngrese los datos del vehiculo:");
-            System.Console.WriteLine("\nMarca:");
+            System.Console.Write("\nMarca:");
             v.Marca = Console.ReadLine()!;
             
-            System.Console.WriteLine("\nPrecio:");
+            System.Console.Write("\nPrecio:");
             v.Precio = decimal.Parse(Console.ReadLine()!);
 
             System.Console.WriteLine("\nResultado:");
             System.Console.WriteLine(v.ToString());
 
-            System.Console.WriteLine("\nPresione cualquier letra para continuar..");
-            Console.ReadKey();
+            if (opcionNumero == 4)
+            {
+            System.Console.WriteLine("Estas seguro que quieres salir?");
+            string respuesta = Console.ReadLine()!;
 
-            Main(args);
+            if (respuesta == "s")
+            {
+                System.Console.WriteLine("Gracias por usar el programa, vuelva pronto!");
+            }
+            System.Console.WriteLine("Programa en ejecucion, presione cualquier letra para continuar...");
+            Console.ReadKey();
+            }
+
+            System.Console.WriteLine("\nPresione cualquier letra para continuar...");
+            Console.ReadKey();
+            }
         }
     }
 }
